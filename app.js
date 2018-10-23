b65f9592813ef4c52aa296fe0633252328e386d0
Nyapal Gatkuoth <nyapalgatkuoth@Nyapals-MacBook-Pro.local>
2018-10-23 15:56:56 -0700

    users can see single review

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const Review = mongoose.model('Review', {
   title: String,
+  rating: Number,
   description: String,
   movieTitle: String
 });
@@ -40,13 +41,22 @@ app.get('/reviews/new', (req, res) => {
 
 // CREATE
 app.post('/reviews', (req, res) => {
-  Review.create(req.body).then((review) => {
-    console.log(review);
-    res.redirect('/');
-  }).catch((err) => {
-    console.log(err.message);
-  })
+    Review.create(req.body).then((review) => {
+      console.log(review)
+      res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
+    }).catch((err) => {
+      console.log(err.message)
+    })
 })
+
+// SHOW
+app.get('/reviews/:id', (req, res) => {
+    Review.findById(req.params.id).then((review) => {
+      res.render('reviews-show', { review: review })
+    }).catch((err) => {
+      console.log(err.message);
+    })
+});
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
